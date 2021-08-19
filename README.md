f333602c5f0f24006d704bf7cb90823739b7afec
sonin <1457065857@qq.com>
2021-08-19 11:27:47 +0800

    初步引入flowable模块

---
+++ README.md
@@ -0,0 +1,7 @@
+#### 端口占用
+
+* kafka-producer: 6000
+* kafka-consumer: 6001
+* dubbo-provider: 6002
+* dubbo-consumer: 6003
+* flowable-server: 6004
---
+++ flowable-server/src/main/java/com/sonin/flowableserver/FlowableServerApplication.java
@@ -0,0 +1,17 @@
+package com.sonin.flowableserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sonin
+ * @date 2021/8/19 10:58
+ */
+@SpringBootApplication
+public class FlowableServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableServerApplication.class, args);
+    }
+
+}
