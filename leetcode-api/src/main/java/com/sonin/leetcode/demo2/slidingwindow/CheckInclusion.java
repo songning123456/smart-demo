5dfa2a3685f5818fed2edd2376b591b5b400812c
sonin <1457065857@qq.com>
2021-09-15 08:20:42 +0800

    滑动窗口

---
+++ leetcode-api/src/main/java/com/sonin/leetcode/demo2/slidingwindow/CheckInclusion.java
@@ -0,0 +1,54 @@
+package com.sonin.leetcode.demo2.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author sonin
+ * @date 2021/9/15 7:59
+ */
+public class CheckInclusion {
+
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0, right = 0;
+        int valid = 0;
+        for (Character item : s1.toCharArray()) {
+            need.put(item, need.getOrDefault(item, 0) + 1);
+        }
+        while (right < s2.length()) {
+            Character rightChar = s2.charAt(right);
+            right++;
+            if (need.containsKey(rightChar)) {
+                window.put(rightChar, window.getOrDefault(rightChar, 0) + 1);
+                if (need.get(rightChar).equals(window.get(rightChar))) {
+                    valid++;
+                }
+            }
+            while (right - left > s1.length()) {
+                if (need.size() == valid) {
+                    return true;
+                }
+                Character leftChar = s2.charAt(left);
+                left++;
+                if (need.containsKey(leftChar)) {
+                    if (need.get(leftChar).equals(window.get(leftChar))) {
+                        valid--;
+                    }
+                    window.put(leftChar, window.get(leftChar) - 1);
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        CheckInclusion checkInclusion = new CheckInclusion();
+        boolean result = checkInclusion.checkInclusion(s1, s2);
+        System.out.println(result);
+    }
+
+}
