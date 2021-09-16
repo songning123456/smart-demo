eb89aa953a605a08922aed9ae561245061f9b5ce
sonin <1457065857@qq.com>
2021-09-16 20:01:52 +0800

    ssmframework框架补充

---
+++ ssmframework-api/src/main/java/com/sonin/demo/service/ModifyService.java
@@ -0,0 +1,12 @@
+package com.sonin.demo.service;
+
+import java.util.Map;
+
+/**
+ * @author sonin
+ * @date 2021/9/16 19:56
+ */
+public interface ModifyService {
+
+    Map<String, Object> modifyByName(String name);
+}
