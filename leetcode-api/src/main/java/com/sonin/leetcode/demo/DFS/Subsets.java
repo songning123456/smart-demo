4b0977e4ef6552e644580632c032e1226e42abba
sonin <1457065857@qq.com>
2021-09-21 07:35:47 +0800

    DFS

---
+++ leetcode-api/src/main/java/com/sonin/leetcode/demo/DFS/Subsets.java
@@ -0,0 +1,36 @@
+package com.sonin.leetcode.demo.DFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author sonin
+ * @date 2021/9/20 17:34
+ */
+public class Subsets {
+
+    private List<List<Integer>> res = new ArrayList<>();
+
+    private void backtrack(int[] nums, int start, LinkedList<Integer> track) {
+        res.add(new ArrayList<>(track));
+        for (int i = start; i < nums.length; i++) {
+            track.add(nums[i]);
+            backtrack(nums, i + 1, track);
+            track.removeLast();
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums, 0, track);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3};
+        Subsets subsets = new Subsets();
+        List<List<Integer>> res = subsets.subsets(nums);
+        System.out.println(res);
+    }
+}
