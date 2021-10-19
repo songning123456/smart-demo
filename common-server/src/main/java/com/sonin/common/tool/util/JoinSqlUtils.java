844daf6b8748b098b33a53ac13835e8bba04b139
sonin <1457065857@qq.com>
2021-10-19 08:14:24 +0800

    优化

--- common-server/src/main/java/com/sonin/common/tool/util/JoinSqlUtils.java
+++ common-server/src/main/java/com/sonin/common/tool/util/JoinSqlUtils.java
@@ -3,8 +3,7 @@ package com.sonin.common.tool.util;
 import com.sonin.common.tool.annotation.JoinSqlAnno;
 
 import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author sonin
@@ -32,9 +31,10 @@ public class JoinSqlUtils {
             if (field.get(object) == null) {
                 field.set(object, field.getType().newInstance());
             }
+            Object srcObject = field.get(object);
+            field.setAccessible(false);
             // 设置唯一主键
             String uuid = UniqIdUtils.getInstance().getUniqID();
-            Object srcObject = field.get(object);
             Field srcField = srcObject.getClass().getDeclaredField(joinSqlAnno.primaryKey());
             srcField.setAccessible(true);
             srcField.set(srcObject, uuid);
@@ -47,7 +47,6 @@ public class JoinSqlUtils {
                 targetField.setAccessible(false);
             }
             class2ObjMap.put(field.getType(), srcObject);
-            field.setAccessible(false);
         }
         class2ObjMap.clear();
     }
@@ -68,43 +67,36 @@ public class JoinSqlUtils {
                 continue;
             }
             field.setAccessible(true);
-            if (field.get(object) == null) {
+            Object srcObject = field.get(object);
+            field.setAccessible(false);
+            if (srcObject == null) {
                 class2ObjMap.clear();
-                field.setAccessible(false);
                 throw new Exception(field.getName() + " NULL POINT EXCEPTION");
             }
-            Object srcObject = field.get(object);
             Field srcField = srcObject.getClass().getDeclaredField(joinSqlAnno.primaryKey());
             srcField.setAccessible(true);
-            if (srcField.get(srcObject) == null) {
+            Object srcFieldVal = srcField.get(srcObject);
+            srcField.setAccessible(false);
+            if (srcFieldVal == null) {
                 class2ObjMap.clear();
-                srcField.setAccessible(false);
-                field.setAccessible(false);
                 throw new Exception(field.getName() + " Primary Key NULL POINT EXCEPTION");
             }
             Object targetObject = class2ObjMap.get(joinSqlAnno.targetClass());
             if (targetObject != null) {
                 Field targetField = targetObject.getClass().getDeclaredField(joinSqlAnno.foreignKey());
                 targetField.setAccessible(true);
-                if (targetField.get(targetObject) == null) {
+                Object targetFieldVal = targetField.get(targetObject);
+                targetField.setAccessible(false);
+                if (targetFieldVal == null) {
                     class2ObjMap.clear();
-                    srcField.setAccessible(false);
-                    targetField.setAccessible(false);
-                    field.setAccessible(false);
                     throw new Exception(joinSqlAnno.targetClass().getName() + " NULL POINT EXCEPTION");
                 }
-                if (!srcField.get(srcObject).equals(targetField.get(targetObject))) {
+                if (!srcFieldVal.equals(targetFieldVal)) {
                     class2ObjMap.clear();
-                    srcField.setAccessible(false);
-                    targetField.setAccessible(false);
-                    field.setAccessible(false);
                     throw new Exception(srcObject.getClass().getSimpleName() + " Primary Key != " + targetObject.getClass().getSimpleName() + " Foreign Key");
                 }
-                targetField.setAccessible(false);
             }
             class2ObjMap.put(field.getType(), srcObject);
-            srcField.setAccessible(false);
-            field.setAccessible(false);
         }
         class2ObjMap.clear();
     }
@@ -121,14 +113,29 @@ public class JoinSqlUtils {
         if (fields.length == 0) {
             throw new Exception("请输入查询对象");
         }
-        String sql = "";
-        String aliasObj = "";
-        for (int i = 0; i < fields.length; i++) {
-            Field field = fields[i];
-            if (i == 0) {
-                field.setAccessible(true);
-            } else {
-
+        List<Object> subObjList = new LinkedList<>();
+        for (Field field : fields) {
+            JoinSqlAnno joinSqlAnno = field.getAnnotation(JoinSqlAnno.class);
+            if (joinSqlAnno == null) {
+                continue;
+            }
+            field.setAccessible(true);
+            Object srcObject = field.get(object);
+            field.setAccessible(false);
+            if (srcObject == null) {
+                continue;
+            }
+            if (!subObjList.contains(srcObject)) {
+                subObjList.add(srcObject);
+            }
+            if (joinSqlAnno.targetClass() != Object.class) {
+                Field targetField = clazz.getDeclaredField(joinSqlAnno.targetClass().getSimpleName());
+                targetField.setAccessible(true);
+                Object targetObject = targetField.get(object);
+                targetField.setAccessible(false);
+                if (!subObjList.contains(targetObject)) {
+                    subObjList.add(targetObject);
+                }
             }
         }
         return "";
