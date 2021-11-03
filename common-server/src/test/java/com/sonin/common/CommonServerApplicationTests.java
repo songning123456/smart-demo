b53de831db027b0eb662a19c9bee718f2c0fe76a
sonin <1457065857@qq.com>
2021-11-03 19:05:12 +0800

    javassist创建类Test

--- common-server/src/test/java/com/sonin/common/CommonServerApplicationTests.java
+++ common-server/src/test/java/com/sonin/common/CommonServerApplicationTests.java
@@ -1,5 +1,8 @@
 package com.sonin.common;
 
+import com.sonin.common.modules.demo.entity.DemoD;
+import javassist.*;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -8,5 +11,19 @@ import org.springframework.boot.test.context.SpringBootTest;
  */
 @SpringBootTest
 public class CommonServerApplicationTests {
+
+    @Test
+    public void createField() throws Exception {
+        ClassPool classPool = ClassPool.getDefault();
+        CtClass ctClass = classPool.get(DemoD.class.getName());
+        String fieldType = String.class.getName();
+        String fieldName = "testField";
+        CtField ctField = new CtField(classPool.get(fieldType), fieldName, ctClass);
+        ctField.setModifiers(Modifier.PRIVATE);
+        ctClass.addField(ctField);
+        Loader classLoader = new Loader(classPool);
+        Class newClazz = classLoader.loadClass(ctClass.getName());
+        System.out.println("");
+    }
 
 }
