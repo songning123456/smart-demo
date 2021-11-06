c7a15220fe608028a034abb06381af3309382027
sonin <1457065857@qq.com>
2021-11-06 17:24:06 +0800

    javassist创建类Test

---
+++ common-server/src/main/java/com/sonin/common/modules/demo/vo/DemoDVO.java
@@ -0,0 +1,12 @@
+package com.sonin.common.modules.demo.vo;
+
+import com.sonin.common.modules.demo.entity.DemoD;
+import lombok.Data;
+
+/**
+ * @author sonin
+ * @date 2021/11/4 9:07
+ */
+@Data
+public class DemoDVO extends DemoD {
+}
--- common-server/src/test/java/com/sonin/common/CommonServerApplicationTests.java
+++ common-server/src/test/java/com/sonin/common/CommonServerApplicationTests.java
@@ -5,6 +5,8 @@ import javassist.*;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.reflect.Method;
+
 /**
  * @author sonin
  * @date 2021/10/2 12:18
@@ -16,14 +18,29 @@ public class CommonServerApplicationTests {
     public void createField() throws Exception {
         ClassPool classPool = ClassPool.getDefault();
         CtClass ctClass = classPool.get(DemoD.class.getName());
-        String fieldType = String.class.getName();
-        String fieldName = "testField";
-        CtField ctField = new CtField(classPool.get(fieldType), fieldName, ctClass);
+        CtClass fieldType = classPool.get(String.class.getName());
+        // 创建field字段
+        CtField ctField = new CtField(fieldType, "fieldName", ctClass);
         ctField.setModifiers(Modifier.PRIVATE);
         ctClass.addField(ctField);
+        // 创建set方法
+        CtMethod setCtMethod = new CtMethod(CtClass.voidType, "setFieldName", new CtClass[]{fieldType}, ctClass);
+        setCtMethod.setModifiers(Modifier.PUBLIC);
+        setCtMethod.setBody("{this.fieldName = $1;}");
+        ctClass.addMethod(setCtMethod);
+        // 创建get方法
+        CtMethod getCtMethod = new CtMethod(fieldType, "getFieldName", new CtClass[]{}, ctClass);
+        getCtMethod.setModifiers(Modifier.PUBLIC);
+        getCtMethod.setBody("{return this.fieldName;}");
+        ctClass.addMethod(getCtMethod);
         Loader classLoader = new Loader(classPool);
-        Class newClazz = classLoader.loadClass(ctClass.getName());
-        System.out.println("");
+        Class NewDemoD = classLoader.loadClass(ctClass.getName());
+        // 测试结果
+        Object newDemoD = NewDemoD.newInstance();
+        Method setMethod = newDemoD.getClass().getDeclaredMethod("setFieldName", String.class);
+        setMethod.invoke(newDemoD, "123");
+        Method getMethod = newDemoD.getClass().getDeclaredMethod("getFieldName");
+        System.out.println(getMethod.invoke(newDemoD));
     }
 
 }
